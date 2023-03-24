cf8d6192e9cbc0ba34c74b856c9380948b12be39
Walter <walternadalin4@gmail.com>
2023-03-25 00:08:07 +0100

    Corrected bar plot and third plot

--- analysis/bar_plot.py
+++ analysis/bar_plot.py
@@ -1,4 +1,4 @@
-from matplotlib.pyplot import subplots, show, title, suptitle
+from matplotlib.pyplot import savefig, subplots, show, title, suptitle
 from numpy import array, zeros, arange
 
 def num(s):
@@ -11,8 +11,9 @@ with open('data/times.txt') as f:
     data = [[num(x) for x in line.split()[1:]] for line in f.readlines()]
 
 nodes = 4
-naive = data[nodes * 2:nodes * 3]
-dgemm = data[nodes * 3:]
+section = 3
+naive = data[nodes * (section - 1):nodes * section]
+dgemm = data[nodes * section:nodes * (section + 1)]
 processors = array([n for _, n, *_ in naive]) // 32
 naive_comm = array([t for *_, t, _ in naive])
 naive_comp = array([t for *_, t in naive])
@@ -34,7 +35,7 @@ for times in measures:
     offset = width * multiplier - width / 2
     
     for time in times:
-        p = ax.bar(processors + offset, time - bottom, width, label = label[i], bottom = bottom, \
+        p = ax.bar(processors + offset, time, width, label = label[i], bottom = bottom, \
                    color = bar_colors[i], edgecolor = 'black')
         bottom += times[0] 
         i += 1
@@ -50,4 +51,6 @@ ax.grid(linestyle = '--', axis = 'y')
 
 title(f"Size of matrices: {n}"+r"$\times$"+f"{n}", fontsize = 10)
 suptitle('Communication and computation times per number of nodes', fontsize = 13, y = 1.03, x = 0.54)
+savefig(f'analysis/{n}.png')
 show()
+
