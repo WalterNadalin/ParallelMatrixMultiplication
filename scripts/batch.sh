21ba9e0b34f9105b278f68affdb141c08c7fbdc7
trb35 user <a08trb35@login01.m100.cineca.it>
2023-03-29 14:56:20 +0200

    new data

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A tra23_units
 #SBATCH -p m100_usr_prod
-#SBATCH --time 00:30:00       # format: HH:MM:SS
+#SBATCH --time 01:00:00       # format: HH:MM:SS
 #SBATCH -N 8                  # nodes
 #SBATCH --ntasks-per-node=32  # tasks out of 128
 #SBATCH --gres=gpu:4          # gpus per node out of 4
@@ -12,25 +12,28 @@
 #SBATCH --mail-user=walter.nadalin@studenti.units.it
 module load autoload spectrum_mpi
 module load autoload openblas
-make
-dim=10000
-prc=32
 
-for _ in {1..4}
+for dim in {5000..25000..5000}
 do
-	mpirun -np $prc ./multiplication.x $dim 
-	((prc*=2))
+	make
+	prc=32
+
+	for _ in {1..4}
+	do
+		mpirun -np $prc -npernode 32 --map-by socket --bind-to core ./multiplication.x $dim 
+		((prc*=2))
+	done
+
+	make clean
+	make flags=dgemm
+	export OMP_NUM_THREADS=1
+	prc=32
+
+	for value in {1..4}
+	do
+		mpirun -np $prc -npernode 32 --map-by socket --bind-to core ./multiplication.x $dim 
+		((prc*=2))
+	done
+
+	make clean
 done
-
-make clean
-make flags=dgemm
-export OMP_NUM_THREADS=1
-prc=32
-
-for value in {1..4}
-do
-	mpirun -np $prc ./multiplication.x $dim 
-	((prc*=2))
-done
-
-make clean
